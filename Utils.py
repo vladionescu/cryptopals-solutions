0a1c6259dcddb11adad034dbf7b4e94ec3e5dde5
Vlad Ionescu <vlad.ionescu@nccgroup.trust>
2017-06-19 18:34:54 -0700

    Added fixed XOR for two hex strings

--- Utils.py
+++ Utils.py
@@ -7,5 +7,16 @@ class Convert(object):
         binary = binascii.unhexlify(hex_string)
         return binascii.b2a_base64(binary)
 
+class Op(object):
+    @staticmethod
+    def xor(hex1, hex2):
+        bin1 = binascii.unhexlify(hex1)
+        bin2 = binascii.unhexlify(hex2)
+
+        xor = "".join([chr(ord(a) ^ ord(b)) for a,b in zip(bin1, bin2)])
+
+        return binascii.hexlify(xor)
+
 if __name__ == "__main__":
     print "This is a module."
+
